e82f7328e7af484b0d131a985b9107ad1695c35f
Dionys Seidel <dionysseidel@gmail.com>
2022-08-26 10:19:08 -0300

    Specialized microservices are now set correctly

--- catalog-service-parcial/src/main/java/com/dh/catalogservice/domain/repository/MovieRepository.java
+++ catalog-service-parcial/src/main/java/com/dh/catalogservice/domain/repository/MovieRepository.java
@@ -1,4 +1,7 @@
 package com.dh.catalogservice.domain.repository;
 
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "movie-service")
 public interface MovieRepository {
 }
