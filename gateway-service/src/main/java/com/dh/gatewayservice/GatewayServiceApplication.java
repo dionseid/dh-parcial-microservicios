87924446b469c4e38296746723a7c535fcfe869a
Dionys Seidel <dionysseidel@gmail.com>
2022-08-26 13:19:08 -0300

    Gateway service is now properly set

--- gateway-service/src/main/java/com/dh/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/dh/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,7 @@ package com.dh.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
