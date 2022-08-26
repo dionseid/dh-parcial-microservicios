db5e6b584d632937e47804f135e6175f2cfd5725
Dionys Seidel <dionysseidel@gmail.com>
2022-08-26 13:46:30 -0300

    Temporarily change movie service port in central config to test load balancing

--- movie-service-parcial/src/main/java/com/dh/movieservice/api/controller/MovieController.java
+++ movie-service-parcial/src/main/java/com/dh/movieservice/api/controller/MovieController.java
@@ -3,15 +3,19 @@ package com.dh.movieservice.api.controller;
 import com.dh.movieservice.api.service.MovieService;
 import com.dh.movieservice.domain.model.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
 	private MovieService movieService;
+	@Value("${server.port}")
+	private int serverPort;
 
 	@Autowired
 	public MovieController(MovieService movieService) {
@@ -19,7 +23,8 @@ public class MovieController {
 	}
 
 	@GetMapping("/{genre}")
-	public ResponseEntity<List<Movie>> getMovieByGenre(@PathVariable String genre) {
+	public ResponseEntity<List<Movie>> getMovieByGenre(@PathVariable String genre, HttpServletResponse response) {
+		response.addHeader("port", String.valueOf(serverPort));
 		return ResponseEntity.ok().body(movieService.getListByGenre(genre));
 	}
 
