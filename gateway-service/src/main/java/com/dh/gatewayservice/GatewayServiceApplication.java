2fffcee6e03f94fb09b2908f82d4f197ccc2bc63
Dionys Seidel <dionysseidel@gmail.com>
2022-09-05 18:20:31 -0300

    Start configuration of Spring Security in gateway-service

--- gateway-service/src/main/java/com/dh/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/dh/gatewayservice/GatewayServiceApplication.java
@@ -3,9 +3,27 @@ package com.dh.gatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
-public class GatewayServiceApplication {
+@RestController
+public class GatewayServiceApplication extends WebSecurityConfigurerAdapter {
+
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+
+		System.out.println(principal);
+
+		return Collections.singletonMap("name", principal.getAttribute("name"));
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
