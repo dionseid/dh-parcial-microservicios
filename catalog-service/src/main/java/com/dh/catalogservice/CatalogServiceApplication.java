40aba354470987075ceb54223a698fa3402ebbba
Dionys Seidel <dionysseidel@gmail.com>
2022-09-09 14:59:00 -0300

    Add annotations to Main classes

--- catalog-service/src/main/java/com/dh/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/dh/catalogservice/CatalogServiceApplication.java
@@ -1,12 +1,13 @@
 package com.dh.catalogservice;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableRabbit
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
--- movie-service/src/main/java/com/dh/movieservice/MovieServiceApplication.java
+++ movie-service/src/main/java/com/dh/movieservice/MovieServiceApplication.java
@@ -1,10 +1,11 @@
 package com.dh.movieservice;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableRabbit
 public class MovieServiceApplication {
 
 	public static void main(String[] args) {
