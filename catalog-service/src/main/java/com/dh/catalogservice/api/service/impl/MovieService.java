c37bf991a73467f3985bba68fc5ecbefa82fdb23
Dionys Seidel <dionysseidel@gmail.com>
2022-09-09 16:06:10 -0300

    Create a queue to post movies from catalog-service

--- catalog-service/src/main/java/com/dh/catalogservice/api/service/impl/MovieService.java
+++ catalog-service/src/main/java/com/dh/catalogservice/api/service/impl/MovieService.java
@@ -7,7 +7,9 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -22,10 +24,14 @@ public class MovieService {
 
     private static final Logger LOG = LoggerFactory.getLogger(MovieService.class);
     private final MovieClient movieClient;
+    @Value("${queue.movie.name}")
+    private String movieQueue;
+    private final RabbitTemplate rabbitTemplate;
 
     @Autowired
-    public MovieService(MovieClient movieClient) {
+    public MovieService(MovieClient movieClient, RabbitTemplate rabbitTemplate) {
         this.movieClient = movieClient;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     @CircuitBreaker(name = "moviesByGenre", fallbackMethod = "moviesByGenreFallback")
@@ -52,6 +58,10 @@ public class MovieService {
 
         return new ResponseEntity<>(new ArrayList<>(), HttpStatus.OK);
 
+    }
+
+    public void save(MovieWS movieDto) {
+        rabbitTemplate.convertAndSend(movieQueue, movieDto); // Pasar el nombre de cola o routing key
     }
 
 
