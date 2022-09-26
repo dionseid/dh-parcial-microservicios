610d44c425b15fb2e1300b690d1c6a907d2ceb2a
Dionys Seidel <dionysseidel@gmail.com>
2022-09-26 07:36:31 -0300

    Modify MovieWS genre property to save a list of strings

--- movie-service/src/main/java/com/dh/movieservice/domain/dto/MovieWS.java
+++ movie-service/src/main/java/com/dh/movieservice/domain/dto/MovieWS.java
@@ -5,11 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder @Data
 public class MovieWS {
     private String name;
-    private String genre;
+    private List<String> genre;
     private String urlStream;
 }
